35925dbdf7a9416846e70844dc21d4e6a92ee211
Kwon Bora <sweetpt88@gmail.com>
2023-11-30 04:15:43 +0900

    redux 2차 수정

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -5,11 +5,11 @@ import Header from "components/Header";
 import Home from "pages/Home";
 import Detail from "pages/Detail";
 import Footer from "components/Footer";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { setLetter } from "redux/modules/letters";
 
 const Router = () => {
-  const letters = useSelector((state) => state.letters);
+  // const letters = useSelector((state) => state.letters);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -18,7 +18,7 @@ const Router = () => {
       dispatch(setLetter(response.data));
     };
     fetchData();
-  }, []);
+  }, [dispatch]);
 
   return (
     <BrowserRouter>
